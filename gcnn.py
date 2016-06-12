fd4fc0da582076fb6e0fb6e3b6ac9ed56cd5efdb
Yash Chandak <yashchandak@yahoo.in>
2016-06-12 23:48:12 +0530

    data interpretting

--- gcnn.py
+++ gcnn.py
@@ -6,3 +6,47 @@ Created on Sat Jun 11 20:58:07 2016
 """
 
 import numpy as np
+
+
+class g_cnn(object):
+    
+    def __init__(self, inp_shape, out_shape, stride = 1, filter_count = 3, size = 3):
+        ##
+        self.stride = stride
+        self.filter_count = filter_count
+        self.size = size
+        self.init_weights()
+    
+    def init_weights(self):
+        ##
+    
+    def forward(self):
+        ##
+    
+    def backprop(self):
+        ##
+        
+
+
+class g_pool(object):
+    def __init__(self):
+        ##book-keeping variable for backprop error
+    
+    def upsample(self):
+        ##
+    
+    def downsample(self):
+        ##create graph with new reduced nodes and edges
+        
+
+
+class activation(object):
+    
+    def __init__(self, fn ='ReLu'):
+        self.fn = fn
+        
+    def activate(self, inp, ):
+        ##RelU and Sigmoid
+    
+    def derivative(self, inp):
+        ##ReLu and Sigmoid
