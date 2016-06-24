668e2afdd0fb459f4a0f1c00959a40d8f80629f8
Yash Chandak <yashchandak@yahoo.in>
2016-06-24 22:17:23 +0530

    completed convolution layer with backprop

--- gcnn.py
+++ gcnn.py
@@ -11,22 +11,30 @@ import networkx as nx
 
 class g_cnn(object):
     
-    def __init__(self, name, inp_shape, out_shape, stride = 1, filter_count = 3, size = 3, prv_count = 1, fn = 'ReLu'):
-        ##
-        self.stride = stride  #not used, for time being
-        self.filter_count = filter_count
-        self.size = size
-        self.filter = np.random.randn(filter_count, size, prv_count)*np.sqrt(2/50)  #TODO instead of 50 put fan-in     
-        self.filter_del = np.zeros(self.filter.shape)        
-        self.bias = np.random.randn(filter_count)  
-        self.bias_del = np.zeros(self.bias.shape)
-        self.temp = np.zeros(size)
-        self.name = name
-        self.adj = [0]*size
-        self.act = activation(fn)
+    def __init__(self, inp_shape, out_shape, stride = 1, filter_count = 3, size = 3, prv_count = 1, fn = 'ReLu'):
+        #Layer variables
+        self.stride         = stride  #not used, for time being
+        self.filter_count   = filter_count
+        self.size           = size
+        self.filter         = np.random.randn(filter_count, size, prv_count)*np.sqrt(2/50)  #TODO instead of 50 put fan-in     
+        self.filter_del     = np.zeros(self.filter.shape)        
+        self.bias           = np.random.randn(filter_count)  
+        self.bias_del       = np.zeros(self.bias.shape)
+        self.temp           = np.zeros(size)
+        self.act            = activation(fn)
+        
+        #Adam weight update variables
+        self.m      = 0
+        self.v      = 0
+        self.beta1  = 0.9
+        self.beta1t = 1
+        self.beta2  = 0.999
+        self.beta2t = 1
+        self.alpha  = 0.001
+        self.eps    = 1e-8
     
     def forward(self, G):
-        #all nodes should have a value under the field 'val' 
+        #for each node, uses its 'val' and computes 'conv'
         nnodes = len(G.keys())
         
         #create dicts to map between actual and temporary node IDs
@@ -39,13 +47,13 @@ class g_cnn(object):
             for j in G[i]['neighbors']:
                 self.adj_mat[node_to_idx[i]][node_to_idx[j]] = 1
         
-        #k powers of adj matrix
+        #powers of adj matrix
         #to find no.of paths between i,j of length 'level'
         adj_mat_pow = np.array([np.identity(nnodes)])        
         for level in range(1,self.size):
             adj_mat_pow.append(adj_mat_pow[level-1].dot(self.adj_mat))
         
-        #keep path at level K(p) only if K(q) == False, for all q < p
+        #keep path at level(i) only if level(j) == False, for all j < i
         adj_mat_pow = adj_mat_pow.astype(bool)
         for level in range(self.size -1, 0):
             temp = np.zeros((nnodes,nnodes)).astype(bool)
@@ -77,57 +85,62 @@ class g_cnn(object):
         for node in range(nnodes):
             self.temp = [np.sum([G[self.idx_to_node[idx]]['val'] \
                                  for idx in self.adj_list[level][node]], \
-                        axis = 0) \
+                                 axis = 0) \
                         for level in range(self.size)] 
             
             G[self.idx_to_node[node]]['conv'] = self.act.activate \
                                                 (np.sum(self.temp * self.filter, axis(1,2)) + self.bias)
         
-            
-#       METHOD WITHOUT ADJ MATRIX COMPUTATON
-#       TODO: DOESNT TAKE INTO ACCOUNT ALL PRV CONVOLUTION LAYER
-#        for node in G.keys():
-#            self.temp.fill(0)
-#            adj[i] = [node]
-#            for i in range(self.size):
-#                if i : adj = list(set([n  for item in adj   for n in G[item]['neighbors'] ])    #neighbors at level i from the node
-#                self.temp[i] += sum([G[item]['val'] for item in adj])                           #sum of neighbor's values
-#                
-#            G[node][self.name] = np.sum(self.temp * self.filter, axis = 1)
-                
-    
-    def backprop(self, G):
-        ##all nodes of G should have 'delta'
+        return G    
 
-        #calculate derivatives of activation for each node
-        for key in G.keys():
-            G[key]['delta'] = self.act.derivative(G[key]['delta'])
-            
+    def backprop(self, G):
+        #for each node, use its 'delta' provided by the pooling layer and computes 'error'
+
         #calculate actual error for the nodes        
         #backprop convolution similar to fwd convolution 
         #since filters are independent of orientation, no flipping is required
-        #instead of 'val'  we use 'delta' given by pooling layer
+        #instead of 'val'  we use 'delta' 
         #instead of 'conv' we use 'error' to store the corresponding errors for the nodes
         for node in range(nnodes):
             self.temp = [np.sum([G[self.idx_to_node[idx]]['delta'] \
                                  for idx in self.adj_list[level][node]], \
-                        axis = 0) \
+                                 axis = 0) \
                         for level in range(self.size)] 
             
             #transpose the filter to go back to dimension of 'prv_count'
-            G[self.idx_to_node[node]]['error'] = np.sum(self.temp * self.filter.T, axis(1,2))  
+            G[self.idx_to_node[node]]['error'] = self.act.derivative(G[self.idx_to_node[node]]['val'])\
+                                                 *np.sum(self.temp * self.filter.T, axis(1,2))  
             
-                
-        ##calculate deltas for filter
+            #calculate deltas for filter
+            self.filter_del += G[self.idx_to_node[node]]['val']*self.temp
+            
+        self.bias_del = np.sum([G[node]['delta'] for node in G.keys()], axis = 0)
         
+        return G
         
-    def update(self):
-        ##add deltas to the weights
+    def update(self, batch_size):
+        #Adam weight update
+        self.filter_del /= batch_size
+        self.bias_del   /= batch_size
+        
+        self.beta1t *= self.beta1
+        self.beta2t *= self.beta2
+        self.m = self.beta1*self.m + (1 - self.beta1)*self.filter_del
+        self.v = self.beta2*self.v + (1 - self.beta2)*(self.filter_del**2)
+        
+        rate = self.alpha*np.sqrt(1 - self.beta2t)/(1 - self.beta1t)
+        self.filter -= rate*self.m/(np.sqrt(self.v) + self.eps)
+        self.bias   -= self.alpha*self.bias_del  #(duh) simple SGD update for bias :P
+        
+        #reset all deltas
+        self.filter_del.fill(0)
 
 
 class g_pool(object):
-    
-    def __init__(self, switch = 1):
+    #TODO check: downsampling is happening after activation but upsampling is done after derivative
+    #idealy upsample should be done before computing derivatives (in case of avg pool)
+    #in case of Max_pool, it's the same either way.
+    def __init__(self, flat = False, switch = False):
         ##book-keeping variable for backprop error
     
     def upsample(self):
@@ -147,6 +160,7 @@ class activation(object):
         self.fn = fn
         
     def activate(self, inp):
+        #clip the outputs, since size of receptive field can vary a lot
         ##RelU/Sigmoid/Softmax
     
     def derivative(self, inp):
@@ -189,7 +203,7 @@ def backprop(net):
 def train_step(net, data):
     #fwd and bwd pass    
     
-def update(net):
+def update(net, batch_size):
     #update the net parameters
 
 def save():
@@ -203,18 +217,19 @@ def train(load_path = False):
     if load_path:
         net = load(load_path)
     else:
-        net = [g_cnn(name = 'G1', filter_count = 4), g_pool(), \
-               g_cnn(name = 'G2', filter_count = 8, prv_count = 4), g_pool(), \
+        net = [g_cnn(filter_count = 4), g_pool() \
+               g_cnn(filter_count = 8, prv_count = 4), g_pool(flat=True), \
                fc_nn(nodes = 256), \
-               fc_nn(nodes =512, dropout = True), \
+               fc_nn(nodes = 512, dropout = True), \
                fc_nn(nodes = class_count, fn="softmax") ]
           
     epoch = 1500
     checkpoint = 25
+    batch_size = 10
     train_error = np.zeros(epoch)
     valid_error = np.zeros(epoch//checkpoint)
     path = ""
-    data = Data(path = path)
+    data = Data(path = path, batch_size = batch_size )
     
     for i in range(epoch):
         while(data.has_more):
@@ -222,7 +237,7 @@ def train(load_path = False):
             db = data.next_batch()
             for d in db:
                 train_step(net, d)                
-            update(net)
+            update(net, batch_size)
             
         if i % checkpoint == 0:
             valid_error = fwd_pass(net, data.training)
@@ -230,3 +245,22 @@ def train(load_path = False):
             
 
 train()
+
+
+
+
+
+
+
+#       METHOD WITHOUT ADJ MATRIX COMPUTATON
+#       TODO: DOESNT TAKE INTO ACCOUNT ALL PRV CONVOLUTION LAYER
+#        for node in G.keys():
+#            self.temp.fill(0)
+#            adj[i] = [node]
+#            for i in range(self.size):
+#                if i : adj = list(set([n  for item in adj   for n in G[item]['neighbors'] ])    #neighbors at level i from the node
+#                self.temp[i] += sum([G[item]['val'] for item in adj])                           #sum of neighbor's values
+#                
+#            G[node][self.name] = np.sum(self.temp * self.filter, axis = 1)
+                
+    
